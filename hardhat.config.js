22d75e25e55d246379ac03d31d7ca5579e57e2c1
eyusufatik <esadyusufatik@gmail.com>
2022-06-11 02:40:21 +0300

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,6 @@
-require('dotenv').config();
+require("dotenv").config();
 // require("@nomiclabs/hardhat-solhint");
 require("@nomiclabs/hardhat-waffle");
-
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -11,24 +10,32 @@ module.exports = {
     compilers: [
       {
         version: "0.8.14",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100,
+          },
+          outputSelection: {
+            "*": {
+              "*": ["storageLayout"],
+            },
+          },
+        },
       },
       {
         version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100,
+          },
+          outputSelection: {
+            "*": {
+              "*": ["storageLayout"],
+            },
+          },
+        }
       },
     ],
-
-    version: "0.8.14",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 100,
-      },
-      outputSelection: {
-        "*": {
-          "*": ["storageLayout"]
-        }
-      }
-    },
   },
-  
 };
