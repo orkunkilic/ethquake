83183bce294eeeb4a1e0b0d2305185066bda223e
orkunkilic <orkun.kilic@boun.edu.tr>
2022-06-10 23:05:43 +0300

    ui lib

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,22 +1,26 @@
 import React from "react"
 import { useNavigate } from "react-router-dom";
+import { GeistProvider, CssBaseline, Button, Text } from '@geist-ui/core'
 
 function App() {
   let navigate = useNavigate();
 
-  const insureHandler = () => {
-    navigate("/insure")
-  }
-
   return (
-    <div style={{
-      display: "flex", alignItems: "center",
-      justifyContent: "center", flexDirection: "column"
-    }}>
-      <button onClick={insureHandler}>Insure Your House</button>
-      <button>Register House</button>
-      <button>Transfer House</button>
-    </div>
+    <GeistProvider>
+    <CssBaseline />
+      <div style={{
+        display: "flex",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center", 
+        flexDirection: "column"
+      }}>
+        <Text h1>De-Insurance</Text>
+        <Button width="300px" mt={3} onClick={() => navigate('/insure')}>Insurance</Button>
+        <Button width="300px" mt={3} onClick={() => navigate('/issue')}>Issue Deed NFT</Button>
+        <Button width="300px" mt={3} onClick={() => navigate('/transfer')}>Transfer Deed</Button>
+      </div>
+    </GeistProvider>
   );
 
 }
