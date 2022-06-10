531bee8bdd08f9ba1998a5677d53ebc0de46e5bd
“boray” <bsaygilier33@gmail.com>
2022-06-10 23:04:02 +0300

    enumerable update

--- contracts/DeedNFT.sol
+++ contracts/DeedNFT.sol
@@ -2,10 +2,10 @@
 
 pragma solidity ^0.8.14;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract DeedNFT is ERC721, AccessControl {    
+contract DeedNFT is ERC721Enumerable, AccessControl {    
     bytes32 public constant MINTER_ROLE = keccak256("NOTARY__ROLE");
     bytes32 public constant STATE_ROLE = keccak256("STATE_ROLE");
     bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
@@ -68,12 +68,21 @@ contract DeedNFT is ERC721, AccessControl {
         _safeMint(to, tokenId);
     }
 
+    function tokenIdsByAddress(address addressToQuery) public view returns(uint256[]  memory) {
+	uint balance_ =  balanceOf(addressToQuery);
+	uint256[] memory tokenIds = new uint256[](balance_);
+	for(uint256 i=0;i<balance_;i++){
+	   tokenIds[i] =  tokenOfOwnerByIndex(addressToQuery,i);
+	}
+	return tokenIds;
+    }
+
     // The following functions are overrides required by Solidity.
 
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, AccessControl)
+        override(ERC721Enumerable, AccessControl)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
