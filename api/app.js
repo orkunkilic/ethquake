be81fee752a200a1e9e07acef61160954cedfc5a
orkunkilic <orkun.kilic@boun.edu.tr>
2022-06-10 22:12:09 +0300

    create contract interactions

--- api/app.js
+++ api/app.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const axios = require('axios')
+const { ethers } = require('dotenv')
+require('dotenv').config()
+
 const app = express()
 const port = 3000
+
+const NFT_ADDRESS = '0x0'
+const Deed_NFT_ABI = require('./DeedNFT.json')
+
+const RPC_URL = process.env.RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 
 app.get('/', async (req, res) => {
     date = new Date()
@@ -17,6 +27,36 @@ app.get('/', async (req, res) => {
         res.send(false)
     }
 }) 
+
+app.post('/nft/issue', async (req, res) => {
+    const provider = new ethers.providers.JsonRpcProvider(RPC_URL)
+    const wallet = new ethers.Wallet(PRIVATE_KEY, provider)
+
+    const contract = new ethers.Contract(NFT_ADDRESS, Deed_NFT_ABI, wallet)
+
+    const unsignedTx = await contract.populateTransaction.mint(req.body)
+
+    const signedTx = await wallet.signTransaction(unsignedTx)
+
+    const tx = await provider.sendTransaction(signedTx)
+
+    res.send(tx.hash)
+})
+
+app.post('nft/transfer', async (req, res) => {
+    const provider = new ethers.providers.JsonRpcProvider(RPC_URL)
+    const wallet = new ethers.Wallet(PRIVATE_KEY, provider)
+
+    const contract = new ethers.Contract(NFT_ADDRESS, Deed_NFT_ABI, wallet)
+
+    const unsignedTx = await contract.populateTransaction.transfer(req.body.owner, req.body.receiver, req.body.tokenId, req.body.ownerSignature, req.body.receiverSignature)
+
+    const signedTx = await wallet.signTransaction(unsignedTx)
+
+    const tx = await provider.sendTransaction(signedTx)
+
+    res.send(tx.hash)
+})
 
 app.listen(port, () => {
   console.log(`Earthquake app listening on port ${port}`)
