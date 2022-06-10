22b9489fd00ddb5124ec389e041c936ec03c41b8
orkunkilic <orkun.kilic@boun.edu.tr>
2022-06-10 19:23:35 +0300

    add transfer functions

--- contracts/DeedNFT.sol
+++ contracts/DeedNFT.sol
@@ -7,8 +7,9 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract DeedNFT is ERC721, AccessControl {    
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant TRANSFER_ADMIN_ROLE = keccak256("TRANSFER_ADMIN_ROLE");
 
-    constructor() ERC721("MyToken", "MTK") {
+    constructor() ERC721("DeedNFT", "DNFT") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
@@ -17,6 +18,18 @@ contract DeedNFT is ERC721, AccessControl {
         _safeMint(to, tokenId);
     }
 
+    function transferFrom(address from, address to, uint256 tokenId) public override {
+        require(hasRole("TRANSFER_ADMIN_ROLE", msg.sender));
+        _transfer(from, to, tokenId);
+        // ?
+    }
+
+    function transfer(address from, address to, uint256 tokenId, bytes32 ownerSignature, bytes32 receiverSignature) public onlyRole(TRANSFER_ADMIN_ROLE) {
+        // Make EIP-712 Compatible
+
+        _transfer(from, to, tokenId);
+    }
+    
     // The following functions are overrides required by Solidity.
 
     function supportsInterface(bytes4 interfaceId)
